025415da7c3216c7ee6d0fc78e99a302c5dafc6f
LAPTOP-UKRVLSUP\ckimmons <ckimmons@usc.edu>
2017-02-27 18:45:06 -0800

    Added basic "hover over object to see type" and rotate objects (note: objects do not fall in place based on their center of gravity, that will need to be added later). The main camera is now a child object of the player, and if a player tries to place an object on an anchorpoint, it doesn't allow them to place it if the x and z bounds of the collider are larger than the anchor point.

---
+++ improbable_cause_demo/Assets/Player Actions/GUIComponents.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GUIComponents : MonoBehaviour {
+    public GameObject objectTypeGameObject;
+    private GUIText objectTypeText;
+
+
+    public GUIText getObjectTypeText()
+    {
+        return objectTypeText;
+    }
+
+    public void Start()
+    {
+        objectTypeText = objectTypeGameObject.GetComponent<GUIText>();
+    }
+}
---
+++ improbable_cause_demo/Assets/Player Actions/RotateObject.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateObject : MonoBehaviour {
+    HeldObject heldObject;
+	// Use this for initialization
+	void Start () {
+        heldObject = GetComponent<HeldObject>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GameObject obj = heldObject.getHeldObject();
+        if (!obj) return;
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            obj.transform.Rotate(obj.transform.rotation.x + 45, obj.transform.rotation.y, obj.transform.rotation.z);
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            obj.transform.Rotate(obj.transform.rotation.x - 45, obj.transform.rotation.y, obj.transform.rotation.z);
+        }
+        else if (Input.GetKeyDown(KeyCode.A))
+        {
+            obj.transform.Rotate(obj.transform.rotation.x, obj.transform.rotation.y, obj.transform.rotation.z + 45);
+        }
+
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            obj.transform.Rotate(obj.transform.rotation.x, obj.transform.rotation.y, obj.transform.rotation.z - 45);
+        }
+
+    }
+}
