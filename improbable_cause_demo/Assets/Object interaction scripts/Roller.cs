589595e58d27d1902ed869653c526172f16aef23
LAPTOP-UKRVLSUP\ckimmons <ckimmons@usc.edu>
2017-03-25 14:27:27 -0700

    Added GridSpawner, which should be used to generate the anchor points on the floor for future levels.

---
+++ improbable_cause_demo/Assets/Object interaction scripts/Roller.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class Roller : MonoBehaviour {
+    public float FORCE = 2.0f;
+    Rigidbody rb;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (!collision.gameObject.CompareTag("Room") && !collision.gameObject.CompareTag("AnchorPoint"))
+        {
+            Vector3 point = collision.contacts[0].point;
+            Vector3 direction = point - transform.position;
+            direction = -direction.normalized;
+            if (rb != null)
+            {
+              //  rb.AddForce(direction * FORCE);
+            }
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
