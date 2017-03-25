a31587bd2f87765a8c867e1b12531a20db950459
James Collins <jamescol@usc.edu>
2017-03-24 23:10:13 -0700

    Box point audio done/ point audio template script

---
+++ improbable_cause_demo/Assets/Audio/AudioOnCollide.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioOnCollide : MonoBehaviour {
+
+    public GameObject BoxThingy;
+    public AudioClip CollideSound;
+    private AudioSource SpawnSound;
+	// Use this for initialization
+	void Start () {
+        SpawnSound = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    void OnCollisionEnter(Collision coll){
+        float vol = Random.Range(0.75f, 1.0f);
+        SpawnSound.PlayOneShot(CollideSound, vol);
+    }
+}
