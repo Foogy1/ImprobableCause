354e640966bb462590fa11a0d5acd67b6ef8e074
FOOGYASUSLAPTOP\Jackson <jdbeaver@usc.edu>
2017-03-26 22:52:51 -0700

    added in a janky thing for crates spawning anchor points on top of selves, and then deleting them when picked up

--- improbable_cause_demo/Assets/Object interaction scripts/Stackable.cs
+++ improbable_cause_demo/Assets/Object interaction scripts/Stackable.cs
@@ -1,12 +1,43 @@
 ﻿using UnityEngine;
 
-public class Stackable : AnchorPoint
+public class Stackable : IUsable
 {
     /* IUsables can be placed on this object */
 
-    protected override void Start()
+	public GameObject spawnableAnchorPointPrefab;
+	public GameObject currentSpawnedAnchorPoint;
+
+    /*protected override void Start()
     {
-        side = SIDE.TOP;
-        rend = GetComponent<Renderer>();
-    }
+       
+    }*/
+
+
+
+	public override void place(GameObject dropLocation, AnchorPoint anchorPoint)
+	{
+		this.anchorPoint = anchorPoint;
+		gameObject.layer = DEFAULT_LAYER;
+		Debug.Log(dropLocation.GetComponent<AnchorPoint>().GetPosition(GetComponent<Renderer>().bounds.size.y));
+		//gameObject.transform.localRotation = dropLocation.transform.localRotation;
+		gameObject.transform.position = dropLocation.GetComponent<AnchorPoint>().GetPosition(GetComponent<Renderer>().bounds.size.y);
+		Vector3 currentCrateLocation = this.transform.position; //(this.transform.position.x, 
+		currentCrateLocation.y += 0.7f;
+		//currentCrateLocation.x += 0.5f;
+		currentSpawnedAnchorPoint = Instantiate (spawnableAnchorPointPrefab, currentCrateLocation, Quaternion.identity);
+	}
+
+	public override void pickUp()
+	{
+		gameObject.layer = IGNORE_RAYCAST_LAYER;
+		if (anchorPoint)
+		{
+			anchorPoint.IsOccupied = false;
+			anchorPoint = null;
+		}
+		if (currentSpawnedAnchorPoint != null) {
+			Destroy (currentSpawnedAnchorPoint);
+		}
+	}
+
 }
