40c6413401e62ef85db5311f408a97ad879707ba
SamCel <scceleste@gmail.com>
2017-03-26 17:55:55 -0700

    This is the main menu folder containing all of the assets & the scene itself

---
+++ improbable_cause_demo/Assets/MainMenu/Scripts/QuitGame.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitGame : MonoBehaviour {
+
+	public void ClickExit()
+
+	{
+		Application.Quit();
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
