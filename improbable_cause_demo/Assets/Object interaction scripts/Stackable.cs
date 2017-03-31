56f6d4fbd736a04c7add2b06c14b7ed1700dca81
Foogy1 <jdbeaver@usc.edu>
2017-03-31 12:02:13 -0700

    Update Stackable.cs

--- improbable_cause_demo/Assets/Object interaction scripts/Stackable.cs
+++ improbable_cause_demo/Assets/Object interaction scripts/Stackable.cs
@@ -1,59 +1,54 @@
-﻿using UnityEngine;
+using UnityEngine;
 
 public class Stackable : IUsable
-{
-    /* IUsables can be placed on this object */
+{    
 
-    public GameObject spawnableAnchorPointPrefab;
-    private GameObject currentSpawnedAnchorPoint;
-    public float offset = 1f;
+	public GameObject spawnableAnchorPointPrefab;
+	private GameObject currentSpawnedAnchorPoint;
+	public float offset = 1f;    
 
-    public GameObject getSpawnedAnchorPoint()
-    {
-        return currentSpawnedAnchorPoint;
-    }
 
-    /*protected override void Start()
-    {
+	public override void Start(){
+		Vector3 currentCrateLocation = this.transform.position; //(this.transform.position.x,
+		currentCrateLocation.y += offset;
+		currentSpawnedAnchorPoint = Instantiate(spawnableAnchorPointPrefab, currentCrateLocation, Quaternion.identity);
+		currentSpawnedAnchorPoint.transform.parent = gameObject.transform;
+	}    
 
-    }*/
-    public override void place(GameObject dropLocation, AnchorPoint anchorPoint)
-    {
-        this.anchorPoint = anchorPoint;
-        gameObject.layer = DEFAULT_LAYER;
-        Debug.Log(dropLocation.GetComponent<AnchorPoint>().GetPosition(GetComponent<Renderer>().bounds.size.y));
-        //gameObject.transform.localRotation = dropLocation.transform.localRotation;
-        gameObject.transform.position = dropLocation.GetComponent<AnchorPoint>().GetPosition(GetComponent<Renderer>().bounds.size.y);
-        Vector3 currentCrateLocation = this.transform.position; //(this.transform.position.x,
-        currentCrateLocation.y += offset;
-        //currentCrateLocation.x += 0.5f;
-        currentSpawnedAnchorPoint = Instantiate(spawnableAnchorPointPrefab, currentCrateLocation, Quaternion.identity);
-    }
+	public GameObject getSpawnedAnchorPoint()
+	{
+		return currentSpawnedAnchorPoint;
+	}    /*protected override void Start()
+   {    }*/
+	public override void place(GameObject dropLocation, AnchorPoint anchorPoint)
+	{
+		this.anchorPoint = anchorPoint;
+		gameObject.layer = DEFAULT_LAYER;
+		Debug.Log(dropLocation.GetComponent<AnchorPoint>().GetPosition(GetComponent<Renderer>().bounds.size.y));
+		//gameObject.transform.localRotation = dropLocation.transform.localRotation;
+		gameObject.transform.position = dropLocation.GetComponent<AnchorPoint>().GetPosition(GetComponent<Renderer>().bounds.size.y);
+		Vector3 currentCrateLocation = this.transform.position; //(this.transform.position.x,
+		currentCrateLocation.y += offset;
+		//currentCrateLocation.x += 0.5f;
+		currentSpawnedAnchorPoint = Instantiate(spawnableAnchorPointPrefab, currentCrateLocation, Quaternion.identity);
+		currentSpawnedAnchorPoint.transform.parent = gameObject.transform;
+	}    
 
-    public override void pickUp()
-    {
-        gameObject.layer = IGNORE_RAYCAST_LAYER;
+	public override void pickUp()
+	{
+		gameObject.layer = IGNORE_RAYCAST_LAYER;        if (currentSpawnedAnchorPoint != null) {
+			if (currentSpawnedAnchorPoint.GetComponent<AnchorPoint> ().IsOccupied) {
+				return;
+			} else {
+				Destroy (currentSpawnedAnchorPoint);
 
-        if (currentSpawnedAnchorPoint != null)
-        {
-            if (currentSpawnedAnchorPoint.GetComponent<AnchorPoint>().IsOccupied)
-            {
-                return;
-            }
-            else
-            {
-                Destroy(currentSpawnedAnchorPoint);
-                
-                    anchorPoint.IsOccupied = false;
-                    anchorPoint = null;
-             }
-                
-        }
-       else if (anchorPoint)
-        {
-            anchorPoint.IsOccupied = false;
-            anchorPoint = null;
-        }
-    }
+				anchorPoint.IsOccupied = false;
+				anchorPoint = null;
+			}
 
+		} else if (anchorPoint) {
+			anchorPoint.IsOccupied = false;
+			anchorPoint = null;
+		}
+	}
 }
