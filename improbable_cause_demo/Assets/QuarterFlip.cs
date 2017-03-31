19206301575cc01c5b15e1e5b1c436eca606bb57
DESKTOP-0E7B6DI\Sakuyal <ferreri@usc.edu>
2017-03-31 13:34:15 -0700

    QuarterFlip and WinScene
    QuarterFlip and WinScene push.

---
+++ improbable_cause_demo/Assets/QuarterFlip.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.SceneManagement;
+
+public class QuarterFlip : MonoBehaviour {
+
+
+	private bool flipTime;
+	// Use this for initialization
+	void Start () {
+		flipTime = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (this.transform.position.y > 20) {
+			Debug.Log ("called");
+			flipTime = false;
+			//EditorSceneManager.LoadScene (WinScene);
+		}
+		if (flipTime == true) {
+			this.transform.Translate (0, .5f, 0, Space.World);
+			this.transform.Rotate (0, 0, 30);
+		}
+	}
+	private void OnCollisionEnter(Collision collision)
+	{
+		IUsable usable = collision.gameObject.GetComponent<IUsable>();
+		if (usable) {
+			flipTime = true;
+		}
+	}
+}
