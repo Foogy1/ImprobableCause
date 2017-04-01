0a94c0c34ec1580e3999f48af41b6605a531bb1c
Gray Bohnen <gbohnen@uccs.edu>
2017-03-31 18:32:44 -0600

    Cuckoo Stuff

---
+++ improbable_cause_demo/Assets/CuckooClock.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CuckooClock : MonoBehaviour {
+
+    Animator anim;
+
+	// Use this for initialization
+	void Start ()
+    {
+        anim = GetComponent<Animator>();
+        Invoke("OpenDoors", 1);
+	}
+
+    public void CloseDoors()
+    {
+        anim.SetBool("isOpen", false);
+    }
+
+    public void OpenDoors()
+    {
+        anim.speed = 1;
+        anim.SetBool("isOpen", true);
+    }
+}
